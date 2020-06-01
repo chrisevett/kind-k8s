cb781b294041d81cd17907514c5dc19398c75b46
Chris Evett <chris.evett@gmail.com>
2020-05-31 21:36:07 -0500

    fix: update setup.sh to add datawire helm repo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# required for helm install step
+helm repo add datawire https://www.getambassador.io
+
 kind create cluster
 
 kubectl create namespace ambassador && helm install ambassador --namespace ambassador datawire/ambassador
